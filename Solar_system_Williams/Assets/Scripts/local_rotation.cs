7f927ed07a4167ffa2145633b79adaa25b096b9b
williamsfacundo <facuwilli@gmail.com>
2022-03-17 09:35:54 -0300

    Added local rotation to planets.

---
+++ Solar_system_Williams/Assets/Scripts/local_rotation.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class local_rotation : MonoBehaviour
+{
+    public float rotationMultiplyer = 1.5f;
+
+    private float rotationValue = 0.0f;
+    private float localRotation = 0.0f;
+    
+    private Vector3 rotationAxes = new Vector3(0.0f, 1.0f, 0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationValue = 10.0f;
+        localRotation = rotationValue * rotationMultiplyer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationAxes * localRotation * Time.deltaTime);        
+    }
+}
