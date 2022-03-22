f04402406aa6cc588f9215c3af179baac776d4c9
williamsfacundo <facuwilli@gmail.com>
2022-03-22 12:36:52 -0300

    Added camera movement.

--- Solar_system_Williams/Assets/Scripts/camera.cs
+++ Solar_system_Williams/Assets/Scripts/camera.cs
@@ -4,54 +4,54 @@ public class camera : MonoBehaviour
 {    
     public GameObject sun = null;
 
-    private float rotationValue = 40.0f;
+    private float rotationValue = 100.0f;
+    private float cameraMoveSpeedValue = 1000f;
         
     private Vector3 cameraInitialRotation = new Vector3(0f, 90f, 0f);
     private Vector3 cameraOffset = new Vector3(1200f, 0f, 0f);
     private Vector3 rotationAxes = new Vector3(0f, 1f, 0f);
+    private Vector3 cameraSunNormalDirection;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {       
         Vector3 rotationValue = cameraInitialRotation - transform.rotation.eulerAngles;
 
-        if (sun != null) 
-        { 
-            transform.position = sun.transform.position - cameraOffset; 
-        }
+        transform.position = sun.transform.position - cameraOffset;
+        transform.LookAt(sun.transform.position);
 
         transform.Rotate(rotationValue);
+
+        CalculateCameraSunNormalDirection();        
     }
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        CalculateCameraSunNormalDirection();
-        if (sun != null) 
+    {       
+        if (Input.GetKey(KeyCode.A))
         {
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                transform.RotateAround(sun.transform.position, rotationAxes, -(rotationValue * Time.deltaTime));
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                transform.RotateAround(sun.transform.position, rotationAxes, rotationValue * Time.deltaTime);
-            }
-            else if (Input.GetKeyDown(KeyCode.W))
-            {
-                
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-
-            }
+            transform.RotateAround(sun.transform.position, rotationAxes, rotationValue * Time.deltaTime);
+            transform.LookAt(sun.transform.position);
+            CalculateCameraSunNormalDirection();
+        }
+        else if (Input.GetKey(KeyCode.D)) 
+        {
+            transform.RotateAround(sun.transform.position, rotationAxes, -(rotationValue * Time.deltaTime));
+            transform.LookAt(sun.transform.position);
+            CalculateCameraSunNormalDirection();
         }        
+        else if (Input.GetKey(KeyCode.W))
+        {            
+            transform.position -= cameraSunNormalDirection * cameraMoveSpeedValue * Time.deltaTime;            
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {            
+            transform.position += cameraSunNormalDirection * cameraMoveSpeedValue * Time.deltaTime;
+        }
     }
 
-    Vector3 CalculateCameraSunNormalDirection() 
-    {        
-        Vector3 normalizedDirection = Vector3.Normalize(transform.position - sun.transform.position);
-
-        return normalizedDirection;
+    void CalculateCameraSunNormalDirection() 
+    {
+        cameraSunNormalDirection = Vector3.Normalize(transform.position - sun.transform.position);        
     }
 }
