32f77cec639ab597f7c27abdfc3b0cf59153ee67
williamsfacundo <facuwilli@gmail.com>
2022-03-29 09:49:49 -0300

    Added camera min and max distance to the sun.

--- Solar_system_Williams/Assets/Scripts/camera.cs
+++ Solar_system_Williams/Assets/Scripts/camera.cs
@@ -6,7 +6,10 @@ public class camera : MonoBehaviour
 
     private float rotationValue = 100.0f;
     private float cameraMoveSpeedValue = 1000f;
-        
+    private float maxDistance = 10000f;
+    private float minDistance = 1000f;
+    private float cameraSphereDistance = 0f;    
+
     private Vector3 cameraInitialRotation = new Vector3(0f, 90f, 0f);
     private Vector3 cameraOffset = new Vector3(1200f, 0f, 0f);
     private Vector3 rotationAxes = new Vector3(0f, 1f, 0f);
@@ -27,7 +30,10 @@ public class camera : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {       
+    {
+        cameraSphereDistance = Vector3.Distance(transform.position, sun.transform.position);
+
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.RotateAround(sun.transform.position, rotationAxes, rotationValue * Time.deltaTime);
@@ -39,15 +45,16 @@ public class camera : MonoBehaviour
             transform.RotateAround(sun.transform.position, rotationAxes, -(rotationValue * Time.deltaTime));
             transform.LookAt(sun.transform.position);
             CalculateCameraSunNormalDirection();
-        }        
-        else if (Input.GetKey(KeyCode.W))
-        {            
-            transform.position -= cameraSunNormalDirection * cameraMoveSpeedValue * Time.deltaTime;            
         }
-        else if (Input.GetKey(KeyCode.S))
-        {            
+
+        if (Input.GetKey(KeyCode.W) && cameraSphereDistance > minDistance)
+        {
+            transform.position -= cameraSunNormalDirection * cameraMoveSpeedValue * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S) && cameraSphereDistance < maxDistance)
+        {
             transform.position += cameraSunNormalDirection * cameraMoveSpeedValue * Time.deltaTime;
-        }
+        }        
     }
 
     void CalculateCameraSunNormalDirection() 
